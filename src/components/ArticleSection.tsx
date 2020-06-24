25885164132af95e0f587c161fa4a12208b11a04
Oluwaseyi Suulola <oluwaseyisuulola@gmail.com>
2020-06-23 22:47:27 -0700

    worked on article and footer section, next is mobile responsiveness

--- src/components/ArticleSection.tsx
+++ src/components/ArticleSection.tsx
@@ -1,9 +1,59 @@
 import React from 'react'
 
+
+import currency from "../assets/images/image-currency.jpg"
+import plane from "../assets/images/image-plane.jpg"
+import restaurant from "../assets/images/image-restaurant.jpg"
+import confetti from "../assets/images/image-confetti.jpg"
+
+const articles = [
+    {
+        title: 'Receive money in any currency with no fees',
+        author: 'Suulola Oluwaseyi',
+        image: currency,
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae voluptas natus iure at reprehenderit rerum ipsam. Quia expedita, minima nihil, officia, tempora non sapiente doloremque quod perspiciatis incidunt ratione ab.'
+    },
+    {
+        title: 'Treat yourself without worry about money',
+        author: 'Suulola Oluwaseyi',
+        image: restaurant,
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae voluptas natus iure at reprehenderit rerum ipsam. Quia expedita, minima nihil, officia, tempora non sapiente doloremque quod perspiciatis incidunt ratione ab.'
+    },
+    {
+        title: 'Take your Easybank card wherever you go',
+        author: 'Suulola Oluwaseyi',
+        image: plane,
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae voluptas natus iure at reprehenderit rerum ipsam. Quia expedita, minima nihil, officia, tempora non sapiente doloremque quod perspiciatis incidunt ratione ab.'
+    },
+    {
+        title: 'Out invite-only Beta accounts are now live!',
+        author: 'Suulola Oluwaseyi',
+        image: confetti,
+        content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae voluptas natus iure at reprehenderit rerum ipsam. Quia expedita, minima nihil, officia, tempora non sapiente doloremque quod perspiciatis incidunt ratione ab.'
+    },
+]
+
 function ArticleSection() {
+    const displayedArticles = articles.map(article => (
+        <div style={{
+            minWidth: '16rem',
+            // height: '100px'
+        }} className="card mb-3 mx-4 p-0 col-12 col-md-2 border-white">
+            <img height={200} src={article.image} className="card-img-top" alt="article" />
+            <p className="card-text mt-3"><small className="text-muted pl-4">By {article.author}</small></p>
+            <div className="card-body pt-0">
+                <h5 className="card-title">{article.title}</h5>
+                <p className="card-text">{article.content}...</p>
+
+            </div>
+        </div>
+    ));
     return (
-        <div>
-            ar
+        <div className="p-5 bg-light">
+            <h4 className="px-3 py-2">Latest Article</h4>
+           <div className="row">
+           {displayedArticles}
+           </div>
         </div>
     )
 }
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,59 @@
 import React from 'react'
 
+import logo from "../assets/images/footlogo.svg"
+import Button from '../common/Button';
+import facebook from "../assets/images/icon-facebook.svg"
+import hamburger from "../assets/images/icon-hamburger.svg"
+import instagram from "../assets/images/icon-instagram.svg"
+import pinterest from "../assets/images/icon-pinterest.svg"
+import twitter from "../assets/images/icon-twitter.svg"
+import youtube from "../assets/images/icon-youtube.svg"
+
+
+const socials = [
+    { name: 'Facebook', image: facebook, },
+    { name: 'Twitter', image: twitter, },
+    { name: 'Pinterest', image: pinterest, },
+    { name: 'Instagram', image: instagram, },
+    { name: 'Youtube', image: youtube, },
+]
+
 function Footer() {
+
+    const socialRow = socials.map(social => (
+        <div className="col">
+            <img src={social.image} alt={social.name} />
+        </div>
+    ))
+
     return (
-        <div>
-            
+        <div style={{
+            backgroundColor: 'hsl(233, 26%, 24%)',
+        }} className="row px-5 py-5 m-0">
+            <div className="col-3">
+                <img src={logo} alt="easybank logo" className="mb-5" />
+                <div style={{
+                    maxWidth: '180px'
+                }} className="row" id="social_icons">
+                    {socialRow}
+                </div>
+
+            </div>
+            <div className="col-2">
+                <p className="text-light">About</p>
+                <p className="text-light">Contact</p>
+                <p className="text-light">Blog</p>
+            </div>
+            <div className="col-2">
+                <p className="text-light">Career</p>
+                <p className="text-light">Support</p>
+                <p className="text-light">Privacy Policy</p>
+            </div>
+            <div className="col d-flex flex-column align-items-end">
+                <Button />
+                <p className="mt-5 text-muted">&copy; Easybank. All Right Reserved.</p>
+            </div>
+
         </div>
     )
 }
